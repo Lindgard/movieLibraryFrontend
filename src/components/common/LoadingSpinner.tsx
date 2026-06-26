835db27a7f1f4a6faa37fa496140a3b3874b94e6
ALP <a.b.lindgard@gmail.com>
2026-06-26 11:18:31 +0200

    created start of small loading-UX component.

---
+++ src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface LoadingSpinnerProps {
+    message?: string;
+}
+
+const LoadingSpinner = ({ message }: LoadingSpinnerProps) => {
+    return (
+        <div className="loading_spinner_wrapper">
+            <div className="spinner">
+                {message && <p className="spinner_text">{message}</p>}
+            </div>
+        </div>
+    )
+}
+
+export default LoadingSpinner
