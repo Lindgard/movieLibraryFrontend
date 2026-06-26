4af0ef6d9a0a02e5098cf1f4bab4fa9aad17619f
ALP <a.b.lindgard@gmail.com>
2026-06-26 11:57:55 +0200

    Implemented the login-component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Button from './components/common/Button'
 import TextInput from './components/common/TextInput';
+import Login from './components/Login/Login';
 
 function App() {
 
@@ -12,7 +13,10 @@ function App() {
     <>
       <h1>Hello, world!</h1>
       <Button onClick={handleClick}>Click me</Button>
+      <br />
       <TextInput label="Enter text" type="text" placeholder='Enter text here' />
+      <br />
+      <Login />
     </>
   )
 }
